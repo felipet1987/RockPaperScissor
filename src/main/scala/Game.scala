3628705b59bfa2d0309495786c80e6b93ca547af
felipe talavera <felipe talavera>
2016-11-12 15:29:12 -0300

    second test:  rock beats scissor

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -5,6 +5,9 @@ object Game {
   def move(left: String, right: String): String = (left,right) match {
     case ("paper", "rock") =>  "win"
     case ("rock", "paper") =>  "lose"
+
+    case ("rock", "scissor") =>  "win"
+    case ("scissor", "rock") =>  "lose"
   }
 
 
--- src/test/scala/GameTest.scala
+++ src/test/scala/GameTest.scala
@@ -9,9 +9,13 @@ class GameSuite extends FunSuite {
 
 
   test("Paper Beats Rock") {
-
     assert(Game.move("paper","rock") == "win")
     assert(Game.move("rock","paper") == "lose")
+  }
+
+  test("Rock Beats Scissor") {
+    assert(Game.move("rock","scissor") == "win")
+    assert(Game.move("scissor","rock") == "lose")
   }
 
 
